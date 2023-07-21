62063bef83a04f79bbf5951a0084ea1e6d7451bc
OscarRico-9711 <134390130+OscarRico-9711@users.noreply.github.com>
2023-07-21 07:54:50 -0500

    add animation
    add animation

--- app/src/main/java/com/example/proyectosgrupo4/MainActivity.java
+++ app/src/main/java/com/example/proyectosgrupo4/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.proyectosgrupo4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.sesion_32);
+
+        TextView textView=findViewById(R.id.textView2);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_out);
+        View myView = findViewById(R.id.textView2);
+        myView.startAnimation(animation);
     }
 }
