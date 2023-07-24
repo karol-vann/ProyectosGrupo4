46603c36d552bbb47aab926c25c3b353785d25e3
NicolasJerez <jereznikolas@gmail.com>
2023-07-24 08:34:03 -0500

    sesión 33

--- app/src/main/java/com/example/proyectosgrupo4/MainActivity.java
+++ app/src/main/java/com/example/proyectosgrupo4/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.proyectosgrupo4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.sesion32);
+        TextView textView = findViewById(R.id.textAnimation);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_out);
+        View myView = findViewById(R.id.textAnimation);
+        myView.startAnimation(animation);
     }
 }
