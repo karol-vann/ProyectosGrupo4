ad04e5f76f5d5106abee2f2ce94d559a51b1d087
Nicolas Fiquitiva Segura <Nico52105@gmail.com>
2023-07-24 07:29:08 -0500

    se carga trabajo en clase

--- app/src/main/java/com/example/proyectosgrupo4/MainActivity.java
+++ app/src/main/java/com/example/proyectosgrupo4/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.proyectosgrupo4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.sesion_32);
+
+        TextView textView=findViewById(R.id.TextoEnPantalla);
+        Animation animation= AnimationUtils
+
     }
 }
