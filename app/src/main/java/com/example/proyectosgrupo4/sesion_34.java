26ed7cdae5c4fa322f15cca3f59ac94eb0293525
OscarRico-9711 <134390130+OscarRico-9711@users.noreply.github.com>
2023-07-26 07:28:14 -0500

    enviar mensajes a el usuario
    enviar mensajes a el usuario

---
+++ app/src/main/java/com/example/proyectosgrupo4/sesion_34.java
@@ -0,0 +1,52 @@
+package com.example.proyectosgrupo4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.util.Log;
+import android.widget.Toast;
+
+public class sesion_34 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sesion_34);
+
+        int edad=36;
+        double altura =1.70;
+        boolean esEstudiante= true;
+        String nombre="Nicolas Fiquitiva Segura";
+
+        TextView twEdad=findViewById(R.id.twEdad);
+        TextView twAltura=findViewById(R.id.twAltura);
+        TextView twEsEstudiante=findViewById(R.id.twEsEstudiante);
+        TextView twNombre=findViewById(R.id.twNombre);
+        TextView twComparacion=findViewById(R.id.twComparacion);
+
+        String comparacion;
+        if ((edad-1)==((altura-1)*50)){
+            comparacion="Verdadero";
+        }
+        else {
+            comparacion="Falso";
+        }
+
+        twEdad.setText("Edad:"+edad);
+        twAltura.setText("Altura:"+altura);
+        twEsEstudiante.setText("Es Estudiante:"+esEstudiante);
+        twNombre.setText("Nombre:"+nombre);
+        twComparacion.setText("Edad vs Altura:"+comparacion);
+
+        Log.d("TAG", "prueba texto");
+    }
+
+
+    public void sumar(View view){
+        Log.e("error","se genero un error");
+        Toast.makeText(sesion_34.this,"texto de prueba", Toast.LENGTH_SHORT).show(); //mensaje al usuario
+
+    }
+}
